9364a16c9f616b2899939c4955ad4abae2fae526
Riyaz Pathan <moh.riyazpathan@gmail.com>
2026-05-25 12:17:57 +0530

    Added Binary Search Code for Rotated Array

--- 01 JavaScript/1 Array - Searching Algorithms/3 Search After Rotation/binary_search_on_rotated.js
+++ 01 JavaScript/1 Array - Searching Algorithms/3 Search After Rotation/binary_search_on_rotated.js
@@ -1,49 +1,56 @@
-function searchRotatedArray(arr, target) {
-  console.log("Array:", arr, "Target:", target);
-  let first = 0;
-  let last = arr.length - 1;
+function search(nums, target) {
+  function binarySearch(nums, target, startIndex, lastIndex) {
+    // console.log("=====================================");
+    // console.log("Start ::", startIndex,"(",nums[startIndex], ") End ::", lastIndex, "(",nums[lastIndex],")");
+    let mid = Math.floor((startIndex + lastIndex) / 2);
+    // console.log("Mid ::", mid, "(", nums[mid], ") ||| Target :: ", target);
 
-  while (first <= last) {
-    const mid = Math.floor(first + (last - first) / 2);
-
-    console.log(
-      "First:", first,
-      "Mid:", mid,
-      "Last:", last,
-      "Value:", arr[mid]
-    );
-
-    // Target found
-    if (arr[mid] === target) {
+    if (nums[mid] == target) {
       return mid;
     }
 
-    // Left half is sorted
-    if (arr[first] <= arr[mid]) {
-
-      // Target lies inside left half
-      if (target >= arr[first] && target < arr[mid]) {
-        last = mid - 1;
-      } else {
-        first = mid + 1;
-      }
-
-    }
-    // Right half is sorted
-    else {
-
-      // Target lies inside right half
-      if (target > arr[mid] && target <= arr[last]) {
-        first = mid + 1;
-      } else {
-        last = mid - 1;
-      }
+    if (nums[startIndex] < nums[mid] && nums[startIndex] <= target && nums[mid] >= target) {
+      // console.log("First Half is sorted and element may be there");
+      lastIndex = mid-1;
+      return binarySearch(nums, target, startIndex, lastIndex);
+    } else if (nums[mid] < nums[lastIndex] && nums[mid] <= target && nums[lastIndex] >= target) {
+      // console.log("Second Half is sorted and element may be there");
+      startIndex = mid+1;
+      return binarySearch(nums, target, startIndex, lastIndex);
+    } else if (nums[startIndex] > nums[mid]) {
+      // console.log("First Half is unsorted");
+      lastIndex = mid-1;
+      return binarySearch(nums, target, startIndex, lastIndex);
+    } else if (nums[mid] > nums[lastIndex]) {
+      // console.log("Second Half is unsorted");
+      startIndex = mid+1;
+      return binarySearch(nums, target, startIndex, lastIndex);
+    } else {
+      return -1;
     }
   }
 
-  return -1;
+  // console.log("nums ::", nums);
+  // console.log("Target ::", target);
+  let firstIndex = 0;
+  let lastIndex = nums.length - 1;
+  // console.log("======> First Index ::", firstIndex, nums[firstIndex], "||| Last Index ::", lastIndex, nums[lastIndex]);
+  return binarySearch(nums, target, firstIndex, lastIndex);
 }
 
-const arr = [4,5,6,7,8,9,0,1,2,3];
+let target;
+// target = 0;
+// target = 1;
+// target = 2;
+// target = 3;
+// target = 4;
+target = 5;
+// target = 6;
+// target = 7;
+// target = 8;
+// target = 9;
+let nums = [];
+// nums = [4, 5, 6, 7, 8, 9, 0, 1, 2, 3];
+nums = [8, 9, 0, 1, 2, 3, 4, 5, 6, 7];
 
-console.log(searchRotatedArray(arr, 1));
+console.log(search(nums, target));
