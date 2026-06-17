0812c3e179ac21f466be9ac4fa3ee0bc72a44038
Riyaz Pathan <moh.riyazpathan@gmail.com>
2026-06-17 23:48:40 +0530

    Added Code and Explaination for MinStack

--- 01_JavaScript/8_Stacks/MinStack.js
+++ 01_JavaScript/8_Stacks/MinStack.js
@@ -1,3 +1,13 @@
+/**
+ * @param {string} val
+ * @return {string}
+ * 
+ * Push ::  Push all the elements in normal stack
+ *          If minStack top is bigger then the element then push into minStack
+ * Pop ::   Pop all element from normal stack
+ *          If popped element equals minstack top then pop from minstack
+ * 
+ */
 class MinStack {
   constructor() {
     this.stack = [];
@@ -5,43 +15,60 @@ class MinStack {
   }
 
   push(val) {
+    console.log("Pushing Value ::", val);
     this.stack.push(val);
-
-    if (this.minStack.length === 0 || val <= this.minStack[this.minStack.length - 1]) {
+    if (this.minStack.length == 0 || val <= this.minStack[this.minStack.length - 1]) {
+      console.log("Pushing Value in Min Stack ::", val);
       this.minStack.push(val);
     }
+    console.log("Stack ::", this.stack);
+    console.log("Stack ::", this.minStack);
   }
 
   pop() {
-    const removed = this.stack.pop();
+    let removed = this.stack.pop();
+    console.log("Popping Value ::", removed);
 
     if (removed === this.minStack[this.minStack.length - 1]) {
-      this.minStack.pop();
+      let top = this.minStack.pop();
+      console.log("Popping Value in Min Stack ::", top);
     }
+    console.log("Stack ::", this.stack);
+    console.log("Stack ::", this.minStack);
   }
 
   top() {
+    console.log("Stack Top ::", this.stack[this.stack.length - 1]);
     return this.stack[this.stack.length - 1];
   }
 
   getMin() {
+    console.log("Min Top ::", this.minStack[this.minStack.length - 1]);
     return this.minStack[this.minStack.length - 1];
   }
 }
 
+//  Your MinStack object will be instantiated and called as such:
 var obj = new MinStack();
-console.log(obj.push(6));
-console.log(obj.push(-2));
-console.log(obj.push(0));
-console.log(obj.push(-3));
-console.log(obj.push(4));
-console.log(obj.getMin());
-console.log(obj.pop());
-console.log(obj.getMin());
-console.log(obj.pop());
-console.log(obj.top());
-console.log(obj.getMin());
-console.log(obj.pop());
-console.log(obj.top());
-console.log(obj.getMin());
-console.log(obj.pop());
+obj.push(3);
+obj.push(2);
+obj.push(1);
+console.log("===========");
+obj.push(1);
+obj.push(1);
+obj.push(3);
+obj.push(5);
+console.log("===========");
+obj.pop();
+obj.pop();
+obj.pop();
+console.log("===========");
+obj.push(11);
+obj.push(8);
+obj.push(6);
+obj.push(9);
+console.log("===========");
+obj.pop();
+obj.pop();
+var param_3 = obj.top();
+var param_4 = obj.getMin();
